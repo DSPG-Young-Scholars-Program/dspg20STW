4e39e665e12efdc90161a31f75d4c12de8271b2a
Sarah McDonald <sm9dv@virginia.edu>
2020-06-18 16:27:49 -0400

    almost finished profiling

--- src/data_profiling_sarah.R
+++ src/data_profiling_sarah.R
@@ -10,22 +10,17 @@ conn <- RPostgreSQL::dbConnect(drv = RPostgreSQL::PostgreSQL(),
 
 tbl <- RPostgreSQL::dbGetQuery(
   conn = conn, 
-  statement = "SELECT * FROM bgt_job.jolts_comparison_2010")
+  statement = "SELECT id FROM bgt_job.jolts_comparison_2010 LIMIT 30")
 
-prof_2010 <- data.frame(variable = colnames(tbl), 
-                        completeness = numeric(length = ncol(tbl)),
-                        validity = numeric(length = ncol(tbl)), 
-                        uniqueness = numeric(length = ncol(tbl)))
 
 # Completeness - completeness is a variable metric, the metric is a percentage, the number 
 # of observations that have values compared to the number of observations that “should”
 # have values (NA (not available) are not counted as a value).
 
 completeness <- function(x){
-    (length(x) - sum(is.na(x)))/length(x)
+    sum(!is.na(x))/length(x)
 }
 
-prof_2010$completeness <- apply(tbl, MARGIN = 2, completeness)
 
 # Uniqueness - uniqueness is a variable metric, it is the number of unique valid 
 # values that have been entered for a variable (NAs are not counted as unique values).
@@ -34,79 +29,72 @@ uniqueness <- function(x){
     sum(!is.na(unique(x)))
 }
 
-prof_2010$uniqueness <- apply(tbl, MARGIN = 2, uniqueness)
 
 #Value validity - value validity is a variable metric, data elements with proper 
 #values have value validity; the metric is the percentage of data elements whose 
 #attributes possess values within the range expected for a legitimate entry (NAs
 #are considered a valid value).
 
-value_validity <- function(x, year){
-  
-  z <- c()
-  id <- tbl$id
-  
-  if(length(unique(id)) == nrow(tbl) & length(unique(nchar(id))) == 1){
-    z <- c(z, 1)
-  } else{
-    z <- c(z, NA)
-  }
-  
-  jobdate <- tbl$jobdate
-  
- if(sum(jobdate %in% seq(ymd(paste(as.character(year), "-01-01", sep = "")), 
-                     ymd(paste(as.character(year), "-12-31", sep = "")), '1 day')) ==nrow(tbl)){
-   z <- c(z, 1)
- } else{
-   z <- c(z, NA)
- }
-  
-  state <- tbl$state
-  if(sum(state %in% state.name) == nrow(tbl)){
-    z <- c(z, 1)
-  }else {
-    z <- c(z, NA)
-  }
-  
-  z <- c(z,rep(NA, 6))
+cols <- c("id","jobdate", "state",   "soc", "socname", "lat", "lon",  "minedu",  "maxedu" )
 
-  
-}
+year = 2011
 
-
-# big function
-
-profile <- function(year){
-  
-  for(i in year){
+for(i in year){
     
-  tbl <<- RPostgreSQL::dbGetQuery(
-    conn = conn, 
-    statement = paste("SELECT * FROM bgt_job.jolts_comparison_", i, " LIMIT 30", sep = ""))
-  
-  prof <<- data.frame(variable = colnames(tbl), 
-             completeness = numeric(length = ncol(tbl)),
-             validity = numeric(length = ncol(tbl)), 
-             uniqueness = numeric(length = ncol(tbl)))
-  
-  prof$completeness <- apply(tbl, MARGIN = 2, completeness)
-  prof$uniqueness <- apply(tbl, MARGIN = 2, uniqueness)
-  
-  prof$validity <- value_validity(tbl, i)
-  
-  assign(paste("tbl", i, sep = ""), tbl, envir = .GlobalEnv)  
-  assign(paste("prof", i, sep = ""), prof, envir = .GlobalEnv)  
+   prof <<- data.frame(variable = cols, 
+                        completeness = numeric(length = length(cols)),
+                        validity = numeric(length = length(cols)), 
+                        uniqueness = numeric(length = length(cols)))
+   for (j in cols){
 
-  
-  
-  }
+      tbl <<- RPostgreSQL::dbGetQuery(
+        conn = conn, 
+        statement = paste("SELECT " , j, " FROM bgt_job.jolts_comparison_", i, sep = ""))
+      
+      prof[prof$variable == j, "completeness"] <- completeness(tbl[, j])
+      
+      prof[prof$variable == j, "uniqueness"] <- uniqueness(tbl[, j])
+      
+      # value validity
+      
+      if(j == "id"){
+        
+        prof[prof$variable == j, "validity"] <- length(unique(tbl[, j]))/length(tbl[, j])
+      
+      } else if (j == "jobdate"){
+        
+        prof[prof$variable == j, "validity"] <- sum(tbl[, j] %in% seq(ymd(paste(as.character(year), "-01-01", sep = "")), 
+                             ymd(paste(as.character(year), "-12-31", sep = "")), '1 day'))/length(tbl[, j])
+        
+      } else if (j == "state"){
+        
+        prof[prof$variable == j, "validity"] <- sum(tbl[, j] %in% c(state.name, "District of Columbia", "Puerto Rico", "Virgin Islands of the U.S.", "Guam", "American Samoa", "Northern Mariana Islands", "Palau"))/length(tbl[, j])
+      
+      } else if (j == "soc"){
+        
+        prof[prof$variable == j, "validity"] <- (sum(nchar(tbl[, j]) == 7, na.rm = TRUE) + sum(is.na(tbl[, j])))/length(tbl[, j])
+      
+      } else if (j == "socname"){
+        
+        prof[prof$variable == j, "validity"]  <- (sum(grepl(pattern = "(.*[A-za-z].*)", tbl[, j][!is.na(tbl[, j])])) + sum(is.na(tbl[, j])))/length(tbl[, j])
+      
+      } else if (j == "lat"){
+        
+        prof[prof$variable == j, "validity"]  <- (sum(!is.na(tbl[, j]) > 0) + sum(is.na(tbl[, j])))/length(tbl[, j])
+      
+      } else if (j == "lon"){
+        
+        prof[prof$variable == j, "validity"]  <- (sum(!is.na(tbl[,j] < 0) ) + sum(is.na(tbl[,j])))/length(tbl[,j])
+        
+      } else if (j == "minedu"|j == "maxedu"){
+        
+        prof[prof$variable == j, "validity"]  <- (sum(tbl[, j][!is.na(tbl[, j])] %in% c(12, 14, 16, 18, 21)) + sum(is.na(tbl[, j])))/length(tbl[, j])
+      }
+      
+
+   }
+   
+  assign(paste("prof", i, sep = ""), prof)  
 }
-
-
-profile(2010:2012)
-# comment
-
-
-# value validity
 
 
