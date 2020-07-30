b745594eca9e213e6a8466821ddf2ec960ec84c8
Sarah McDonald <sm9dv@virginia.edu>
2020-07-30 16:18:10 -0400

    discrate per diff

--- src/shiny-dashboard/stwFluid/DashboardSTW.R
+++ src/shiny-dashboard/stwFluid/DashboardSTW.R
@@ -334,21 +334,33 @@ server <- function(input, output) {
   
   #Rendering statebins plot
   output$statebins <- renderPlot({
+  
     
-    #renderPlot height and width
     data <- read.csv("state_year.csv")
     
     viz_data <- data %>% filter(year == input$slide) 
     
-    statebins(viz_data, state_col = "state", value_col = "per_change", direction = 1, round = TRUE, 
-              name = "Percent Change", font_size = 5) + 
-      theme_statebins() +
-      scale_fill_gradient(low = "#0E879C", high = "white", na.value = "grey60", limits = c(-100, 0)) +
+    
+    
+    mutate(viz_data, value =  ifelse(per_change <= 0 & per_change > -20, "[0, -20)",
+                                    ifelse(per_change <= -20 & per_change > -40, "[-20, -40)", 
+                                           ifelse(per_change <= -40 & per_change > -60, "[-40, -60)", 
+                                                  ifelse(per_change <= -60 & per_change > -80, "[-60, -80)", 
+                                                         ifelse(per_change <= -80 & per_change > -100, "[-80, -100)", NA)))))) %>%
+      statebins(ggplot2_scale_function = scale_fill_manual,
+                font_size = 5, 
+                round = TRUE,
+                values = c("[0, -20)" = cbPalette[6], "[-20, -40)" = cbPalette[3],"[-40, -60)" = cbPalette[1], 
+                           "[-60, -80)" = cbPalette[2], "[-80, -100)"= cbPalette[7] )) +
+      theme_statebins()+
       theme(plot.margin = margin(0,0,0,0),
             legend.position = c(.35, .9),
             legend.justification = c("right", "top"),
             legend.direction =  "horizontal") + 
-      labs(fill = "Percent Change")
+      labs(fill = "Percent Difference", title = "2019 BGT/JOLTS Percent Difference by State")
+    
+    
+  
   })
   
   
